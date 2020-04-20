c81167a2959a862f165a7752a8bde121c64e6675
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2020-04-19 22:03:07 -0300

    add listview

--- app/src/main/java/br/com/luizmoratelli/api_android/MainActivity.java
+++ app/src/main/java/br/com/luizmoratelli/api_android/MainActivity.java
@@ -2,11 +2,15 @@ package br.com.luizmoratelli.api_android;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import br.com.luizmoratelli.api_android.model.Censos;
 import br.com.luizmoratelli.api_android.model.CensosResponse;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,6 +19,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        listView = findViewById(R.id.listView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,17 +42,22 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(Call<CensosResponse> call,
                                            Response<CensosResponse> response) {
-                        final TextView textView =
-                                (TextView) findViewById(R.id.textView);
-                        textView.setText(response.body().getEmbedded().getCensos().get(0).getLinks().getSelf().getHref());
+                        final List<Censos> censos = response.body().getEmbedded().getCensos();
+
+                        System.out.println(censos.size());
+                        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, censos);
+                        listView.setAdapter(adapter);
+                        //final TextView textView =
+                          //      (TextView) findViewById(R.id.textView);
+                        //textView.setText(response.body().getEmbedded().getCensos().get(0).getLinks().getSelf().getHref());
                     }
 
                     @Override
                     public void onFailure(Call<CensosResponse> call,
                                           Throwable t) {
-                        final TextView textView =
-                                (TextView) findViewById(R.id.textView);
-                        textView.setText("Erro: " + t.getMessage());
+                        //final TextView textView =
+                        //        (TextView) findViewById(R.id.textView);
+                        //textView.setText("Erro: " + t.getMessage());
                     }
                 });
             }
